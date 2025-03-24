22009f59b970fcfab71ee06970416516ce869ccc
Luiz Mello <contato@luizmello.dev>
2025-03-24 10:56:15 -0300

    add base project

---
+++ coffee-hunter/coffee_hunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffee_hunterApp.swift
+//  coffee-hunter
+//
+//  Created by Luiz Mello on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct coffee_hunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
